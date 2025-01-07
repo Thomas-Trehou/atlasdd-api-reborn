930cb9e829448561effde4d00888de24d681797a
Thomas TREHOU <thomas.trehou@apside.com>
2025-01-07 16:32:10 +0100

    add seeding files

---
+++ data/seed_skill.sql
@@ -0,0 +1,20 @@
+INSERT INTO "skills" ("name")
+VALUES
+    ('Acrobaties'),
+    ('Arcanes'),
+    ('Athlétisme'),
+    ('Discrétion'),
+    ('Dressage'),
+    ('Escamotage'),
+    ('Histoire'),
+    ('Intimidation'),
+    ('Investigation'),
+    ('Médecine'),
+    ('Nature'),
+    ('Perception'),
+    ('Perspicacité'),
+    ('Persuasion'),
+    ('Religion'),
+    ('Représentation'),
+    ('Survie'),
+    ('Tromperie');
