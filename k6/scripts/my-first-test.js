e293bbc5368425b5b6084f90b74f9d0c43ed0e00
Thomas TREHOU <thomas.trehou@apside.com>
2025-02-18 17:22:22 +0100

    init k6 first test

---
+++ k6/scripts/my-first-test.js
@@ -0,0 +1,11 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export const options = {
+    iterations: 10,
+};
+
+export default function () {
+    http.get('https://test-api.k6.io');
+    sleep(1);
+}
