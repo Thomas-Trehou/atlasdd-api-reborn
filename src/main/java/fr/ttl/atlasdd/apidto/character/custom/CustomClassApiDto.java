5737c2f62d48de89eaeefad4a23d5bb10ef5a08c
Thomas-Trehou <thomastrehou.pro@gmail.com>
2025-06-20 10:46:45 +0200

    implement spellcasting ability and type for custom character

--- src/main/java/fr/ttl/atlasdd/apidto/character/custom/CustomClassApiDto.java
+++ src/main/java/fr/ttl/atlasdd/apidto/character/custom/CustomClassApiDto.java
@@ -21,6 +21,12 @@ public class CustomClassApiDto extends BaseApiDto {
     private String name;
 
     @NotNull
+    private String spellcasterType;
+
+    @NotNull
+    private String spellcastingAbility;
+
+    @NotNull
     private String hitDice;
 
     @NotNull
--- src/main/java/fr/ttl/atlasdd/entity/character/custom/CustomClass.java
+++ src/main/java/fr/ttl/atlasdd/entity/character/custom/CustomClass.java
@@ -14,6 +14,8 @@ import java.util.List;
 @Table(name = "custom_classes")
 public class CustomClass extends BaseEntity {
 
+    private String spellcasterType;
+    private String spellcastingAbility;
     private String name;
     private String hitDice;
     private int startingHitPoints;
