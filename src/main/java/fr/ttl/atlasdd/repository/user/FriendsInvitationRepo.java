856eafb5441c9aa5fc30c31657027a28e6130bba
Thomas-Trehou <thomastrehou.pro@gmail.com>
2025-07-11 10:12:44 +0200

    fix state error on friends invitation

--- src/main/java/fr/ttl/atlasdd/repository/user/FriendsInvitationRepo.java
+++ src/main/java/fr/ttl/atlasdd/repository/user/FriendsInvitationRepo.java
@@ -3,6 +3,8 @@ package fr.ttl.atlasdd.repository.user;
 import fr.ttl.atlasdd.entity.user.FriendInvitation;
 import fr.ttl.atlasdd.utils.user.InvitationStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -16,5 +18,6 @@ public interface FriendsInvitationRepo extends JpaRepository<FriendInvitation, L
 
     FriendInvitation findByIdAndRequestUser_Id(Long id, Long senderId);
 
-    List<FriendInvitation> findByReceiverUserIdOrRequestUserIdAndStatus(Long receiverId, Long senderId, InvitationStatus status);
+    @Query("SELECT f FROM FriendInvitation f WHERE (f.requestUser.id = :userId OR f.receiverUser.id = :userId) AND f.status = :status")
+    List<FriendInvitation> findByUserAndStatus(@Param("userId") Long userId, @Param("status") InvitationStatus status);
 }
